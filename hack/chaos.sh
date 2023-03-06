b4f9664f646cfe73dbb891dcc3e438260184f4bd
Vedran Lerenc <vlerenc@gmail.com>
2023-03-06 14:23:18 +0100

    Fix script

--- hack/chaos.sh
+++ hack/chaos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-rm "$(dirname "$0")/../chaostoolkit.log" | true
+rm "$(dirname "$0")/../chaostoolkit.log" &> /dev/null || true
 PYTHONPATH="$(dirname "$0")/.." chaos run --rollback-strategy always "$(dirname "$0")/experiments/${1%.json}.json"
